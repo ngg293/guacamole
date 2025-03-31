9fd1b2f7c311e3068e2ffe87702632a3790d9dfc
ngg293 <161196868+ngg293@users.noreply.github.com>
2025-03-31 22:44:22 +0900

    Update prepare.sh
    한글화 및 수정1

--- prepare.sh
+++ prepare.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 #
-# check if docker is running
+# 도커가 구동중인지 확인합니다
 if ! (docker ps >/dev/null 2>&1)
 then
-	echo "docker daemon not running, will exit here!"
+	echo "도커 프로그램이 실행중이지 않거나 권한이 부족합니다."
 	exit
 fi
-echo "Preparing folder init and creating ./init/initdb.sql"
+echo "폴더 초기화 및 생성 ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
 mkdir -p ./nginx/ssl >/dev/null 2>&1
 chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
-echo "done"
-echo "Preparing folder record and set permissions"
+echo "기록 폴더 생성 및 권한부여"
 mkdir ./record >/dev/null 2>&1
 chmod -R 777 ./record
-echo "done"
-echo "Creating SSL certificates"
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
-echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
-echo "done"
+echo "준비가 끝났습니다."
